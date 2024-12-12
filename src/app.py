82475c0817b53483c3ccc209edab37c5f2e4e75b
Dalyn Lara <77414220+DalynLC@users.noreply.github.com>
2024-12-12 04:10:43 +0000

    primera parte del proyecto completada

--- src/app.py
+++ src/app.py
@@ -2,6 +2,27 @@ import os
 import pandas as pd
 import seaborn as sns
 from dotenv import load_dotenv
+import spotipy
+from spotipy import SpotifyClientCredentials
 
 # load the .env file variables
 load_dotenv()
+
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
+artist_id = "5na1LmEmK2VzNLje9snJYW"
+canciones = []
+popularidad = []
+duracion = []
+
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=client_id,client_secret=client_secret))
+results = spotify.artist_top_tracks(artist_id)
+
+for track in results['tracks'][:10]:
+    print('track    : ' + track['name'])
+    canciones.append(track['name'])
+    popularidad.append(track['popularity'])
+    duracion.append(track['duration_ms']/(1000*60)%60)
+
+print(canciones)
