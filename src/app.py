beabebea571c720525f9434782c6eb9186e82c33
Dalyn Lara <77414220+DalynLC@users.noreply.github.com>
2024-12-13 02:51:29 +0000

    Proyecto completado

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ load_dotenv()
 client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
 
-artist_id = "5na1LmEmK2VzNLje9snJYW"
+artist_id = "6RHTUrRF63xao58xh9FXYJ"
 canciones = []
 popularidad = []
 duracion = []
@@ -20,9 +20,22 @@ spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(cl
 results = spotify.artist_top_tracks(artist_id)
 
 for track in results['tracks'][:10]:
-    print('track    : ' + track['name'])
     canciones.append(track['name'])
     popularidad.append(track['popularity'])
     duracion.append(track['duration_ms']/(1000*60)%60)
 
-print(canciones)
+datos = {
+    'Titles': canciones,
+    'Popularidad': popularidad,
+    'Duracion': duracion
+}
+
+df = pd.DataFrame(datos)
+df = df.sort_values(by = 'Popularidad')
+print(df)
+
+scatter_plot = sns.scatterplot(data = df, y ='Popularidad', x = 'Duracion')
+
+# Etiquetas y título
+fig = scatter_plot.get_figure()
+fig.savefig("scatter_plot1.png")
